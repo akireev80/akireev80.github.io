de0d2427a5da5d3ccd740c4746bd8c11071acfa4
akireev80 <a.kireev80@rambler.ru>
2026-08-29 19:34:22 +0300

    Add files via upload

---
+++ watch-n-learn-demo/_next/static/BaEZHi2MEdaupyTUr1U7s/_clientMiddlewareManifest.js
@@ -0,0 +1,6 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^\\/watch-n-learn-demo(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!api|_next\\/static|_next\\/image|favicon.ico|.*\\.mp4$|.*\\.png$|.*\\.jpg$|.*\\.webp$).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
+    "originalSource": "/((?!api|_next/static|_next/image|favicon.ico|.*\\.mp4$|.*\\.png$|.*\\.jpg$|.*\\.webp$).*)"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
